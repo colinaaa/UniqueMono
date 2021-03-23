3ebeaa591591b236e5a27fbc339b419c6c081647
Colin <colinwang.0616@gmail.com>
2021-03-23 22:51:54 +0800

    feat(utils): add some type helpers
    used for storage package

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -1,4 +1,6 @@
-export type UnknowObject = Record<string | symbol, unknown>;
+export type { NonStringValueKeysOf, StringValueKeysOf } from './typeHelper';
+
+export type UnknownObject = Record<string | symbol, unknown>;
 
 export const EMPTY_OBJ: { readonly [key: string]: unknown } = Object.freeze({});
 export const EMPTY_ARR = Object.freeze([]);
@@ -7,12 +9,12 @@ export const NOOP = (): void => {};
 
 export const isString = (v: unknown): v is string => typeof v === 'string';
 export const isNumber = (v: unknown): v is number => typeof v === 'number';
-export const isObject = (val: unknown): val is UnknowObject => val !== null && typeof val === 'object';
+export const isObject = (val: unknown): val is UnknownObject => val !== null && typeof val === 'object';
 export const isDate = (val: unknown): val is Date => val instanceof Date;
 export const isArray = Array.isArray;
 export const isMap = (val: unknown): val is Map<unknown, unknown> =>
   Object.prototype.toString.call(val) === '[object Map]';
 export const isSet = (val: unknown): val is Set<unknown> => Object.prototype.toString.call(val) === '[object Set]';
 
-export const hasOwn = (val: UnknowObject, key: string | symbol): key is keyof typeof val =>
+export const hasOwn = (val: UnknownObject, key: string | symbol): key is keyof typeof val =>
   Object.prototype.hasOwnProperty.call(val, key);
