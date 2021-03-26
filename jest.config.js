c93bafc99696d48ab55ebe774365ed834af00eae
Colin <colinwang.0616@gmail.com>
2021-03-26 14:50:33 +0800

    feat: ignore node_modules in coverage

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
+/**
+ * @type {import('@jest/types').Config}
+ */
 module.exports = {
   preset: 'ts-jest',
   testPathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/dist/'],
   coverageProvider: 'v8',
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/dist/'],
+  coveragePathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/dist/', '<rootDir>/node_modules/'],
   coverageReporters: ['lcov', 'json'],
 };
