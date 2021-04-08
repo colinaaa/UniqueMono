855e15fb0e5a93ea4b37c56db2d0d51e214f40b0
Colin <colinwang.0616@gmail.com>
2021-04-08 17:44:20 +0800

    feat(api): init api package

--- packages/api/__tests__/api.test.ts
+++
@@ -1,8 +0,0 @@
-import api from '../src/api';
-
-describe('@uniqs/api', () => {
-  it('needs tests', () => {
-    api();
-    expect(0).toBe(0);
-  });
-});
--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -1,3 +1,93 @@
-export default function api(): void {
-  console.log('api');
+import { Evaluation, Gender, Grade, Group, GroupOrTeam, Period, Rank, Status, Step } from '@uniqs/config';
+
+export interface CandidateForm {
+  id: string;
+  name: string;
+  gender: Gender;
+  grade: Grade;
+  institute: string;
+  major: string;
+  rank: Rank;
+  mail: string;
+  phone: string;
+  group: Group;
+  intro: string;
+  isQuick: boolean;
+  referrer?: string;
+  resume?: string;
 }
+
+export interface Candidate<T = Date> extends CandidateForm {
+  updatedAt: T;
+  id: string;
+  abandoned: boolean;
+  rejected: boolean;
+  step: Step;
+  interviewSelections: Interview<T>[];
+  interviewAllocations: {
+    group?: T;
+    team?: T;
+  };
+  comments: Comment[];
+}
+
+export interface Comment {
+  id: string;
+  user: User;
+  content: string;
+  evaluation: Evaluation;
+}
+
+export interface User {
+  id: string;
+  weChatID: string;
+  name: string;
+  password?: string;
+  joinTime: string;
+  isCaptain: boolean;
+  isAdmin: boolean;
+  phone: string;
+  mail?: string;
+  gender: Gender;
+  group: Group;
+  avatar?: string;
+}
+
+export interface Interview<T = Date> {
+  id: string;
+  date: T;
+  period: Period;
+  name: GroupOrTeam;
+  slotNumber: number;
+}
+
+export interface Recruitment<T = Date> {
+  id: string;
+  name: string;
+  beginning: T;
+  deadline: T;
+  end: T;
+  interviews: Interview<T>[];
+  statistics?: Record<Group, Record<Step, number | undefined> | undefined>;
+}
+
+export interface Message {
+  isSelf: boolean;
+  name: string;
+  time: number;
+  isImage: boolean;
+  avatar: string;
+  content: string;
+}
+
+interface SuccessResponse<T> {
+  status: Status.success | Status.info;
+  payload: T;
+}
+
+interface FailureResponse {
+  status: Status.warning | Status.error;
+  message: string;
+}
+
+export type R<T = undefined> = SuccessResponse<T> | FailureResponse;
